5bdce68f54561594deea330e787c0c4f0418b7a2
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-20 23:05:27 -0300

    feat: cria lógica de busca de um planeta específico

--- main.js
+++ main.js
@@ -4,20 +4,40 @@ const fetchPlanets = () => {
     fetch('https://swapi.dev/api/planets/')
         .then((data) => data.json())
         .then((response) => {
-            response.results.forEach((planet) => {
+            response.results.forEach((planet, index) => {
                 const li = document.createElement('li');
                 const card = `<div class="card">
                                 <div class="header">
                                 ${planet.name}
                                 </div>
                                 <div class="body">
-                                <button class="btn btn-primary">See More</button>
+                                <button onClick="fetchSinglePlanet(${index})" class="btn btn-primary">See More</button>
                                 </div>
                               </div>`
                 li.innerHTML = card;
                 ul.appendChild(li);
             })
         })
+}
+
+const fetchSinglePlanet = (index) => {
+    const id = index + 1;
+    const url = `https://swapi.dev/api/planets/${id}`;
+    const ul = document.getElementById('list-planets')
+    fetch(url).then((data) => data.json()).then((response) => {
+        ul.innerHTML = ''; 
+        const li = document.createElement('li');
+        const card = `
+                        <div id="single-planet">
+                            <h1>${response.name}</h1>
+                            <p>Climate: ${response.climate}</p>
+                            <p>Population: ${response.population}</p>
+                            <p>Terrain: ${response.terrain}</p>
+                        </div>
+                    `
+        li.innerHTML = card;
+        ul.appendChild(li)
+    })
 }
 
 window.onload = () => {
